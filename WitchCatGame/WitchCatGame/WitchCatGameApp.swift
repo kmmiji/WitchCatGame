2b52cc0110145b8367b7d3a4637d716f986a3573
Jerry_miji <kmmiji.953@gmail.com>
2023-08-23 01:14:32 +0900

    다시 갈아엎음

---
+++ WitchCatGame/WitchCatGame/WitchCatGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WitchCatGameApp.swift
+//  WitchCatGame
+//
+//  Created by 김미지 on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WitchCatGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
